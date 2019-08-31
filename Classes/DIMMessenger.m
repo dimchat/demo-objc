fc17d4dd83a68d57acc391e4b08b175e0aebbe0d
Moky <albert.moky@gmail.com>
2019-09-01 03:09:35 +0800

    support Meta protocol & Forward message in Messenger

--- Classes/DIMMessenger.m
+++ Classes/DIMMessenger.m
@@ -8,7 +8,7 @@
 
 #import "NSObject+Singleton.h"
 
-#import "DIMFacebook.h"
+#import "DIMFacebook+Storage.h"
 #import "DIMKeyStore.h"
 
 #import "DIMMessenger.h"
@@ -27,6 +27,50 @@ SingletonImplementations(DIMMessenger, sharedInstance)
         _keyCache = [DIMKeyStore sharedInstance];
     }
     return self;
+}
+
+- (DIMInstantMessage *)verifyAndDecryptMessage:(DIMReliableMessage *)rMsg {
+    // 0. [Meta Protocol] check meta in first contact message
+    DIMID *sender = [_barrack IDWithString:rMsg.envelope.sender];
+    DIMMeta *meta = [_barrack metaForID:sender];
+    if (!meta) {
+        // first contact, try meta in message package
+        meta = MKMMetaFromDictionary(rMsg.meta);
+        if (!meta) {
+            // TODO: query meta for sender from DIM network
+            NSAssert(false, @"failed to get meta for sender: %@", sender);
+            return nil;
+        }
+        NSAssert([meta matchID:sender], @"meta not match: %@, %@", sender, meta);
+        DIMFacebook *facebook = [DIMFacebook sharedInstance];
+        if (![facebook saveMeta:meta forID:sender]) {
+            NSAssert(false, @"save meta error: %@, %@", sender, meta);
+            return nil;
+        }
+    }
+    
+    // 1. verify and decrypt
+    DIMInstantMessage *iMsg = [super verifyAndDecryptMessage:rMsg];
+    
+    // 2. check: top-secret message
+    if (iMsg.delegate == nil) {
+        iMsg.delegate = self;
+    }
+    NSAssert(iMsg.content, @"content cannot be empty");
+    if (iMsg.content.type == DKDContentType_Forward) {
+        // do it again to drop the wrapper,
+        // the secret inside the content is the real message
+        DIMForwardContent *content = (DIMForwardContent *)iMsg.content;
+        rMsg = content.forwardMessage;
+        
+        DIMInstantMessage *secret = [self verifyAndDecryptMessage:rMsg];
+        if (secret) {
+            return secret;
+        }
+        // FIXME: not for you?
+    }
+    
+    return iMsg;
 }
 
 @end
