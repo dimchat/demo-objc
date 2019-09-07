a86dd45035f0707eeb4855c1906f990cc453c3bc
Moky <albert.moky@gmail.com>
2019-09-07 20:33:35 +0800

    cache meta, even it's not found

--- Classes/database/DIMMetaTable.m
+++ Classes/database/DIMMetaTable.m
@@ -10,23 +10,27 @@
 
 #import "DIMMetaTable.h"
 
-//typedef NSMutableDictionary<DIMID *, DIMMeta *> CacheTableM;
-//
-//@interface DIMMetaTable () {
-//
-//    CacheTableM *_caches;
-//}
-//
-//@end
+typedef NSMutableDictionary<DIMID *, DIMMeta *> CacheTableM;
+
+@interface DIMMetaTable () {
+
+    CacheTableM *_caches;
+    
+    DIMMeta *_emptyMeta;
+}
+
+@end
 
 @implementation DIMMetaTable
 
-//- (instancetype)init {
-//    if (self = [super init]) {
-//        _caches = [[CacheTableM alloc] init];
-//    }
-//    return self;
-//}
+- (instancetype)init {
+    if (self = [super init]) {
+        _caches = [[CacheTableM alloc] init];
+        
+        _emptyMeta = [[DIMMeta alloc] initWithDictionary:@{}];
+    }
+    return self;
+}
 
 /**
  *  Get meta filepath in Documents Directory
@@ -44,14 +48,14 @@
     return [dir stringByAppendingPathComponent:@"meta.plist"];
 }
 
-//- (BOOL)_cacheMeta:(DIMMeta *)meta forID:(DIMID *)ID {
-//    if (![meta matchID:ID]) {
-//        NSAssert(false, @"meta not match ID: %@, %@", ID, meta);
-//        return NO;
-//    }
-//    [_caches setObject:meta forKey:ID];
-//    return YES;
-//}
+- (BOOL)_cacheMeta:(DIMMeta *)meta forID:(DIMID *)ID {
+    if (![meta matchID:ID]) {
+        NSAssert(false, @"meta not match ID: %@, %@", ID, meta);
+        return NO;
+    }
+    [_caches setObject:meta forKey:ID];
+    return YES;
+}
 
 - (nullable DIMMeta *)_loadMetaForID:(DIMID *)ID {
     NSString *path = [self _filePathWithID:ID];
@@ -65,15 +69,23 @@
 }
 
 - (nullable DIMMeta *)metaForID:(DIMID *)ID {
-//    DIMMeta *meta = [_caches objectForKey:ID];
-//    if (meta) {
-//        return meta;
-//    }
-    DIMMeta *meta = [self _loadMetaForID:ID];
-//    if (meta) {
-//        // no need to check meta again
-//        [_caches setObject:meta forKey:ID];
-//    }
+    DIMMeta *meta = [_caches objectForKey:ID];
+    if (meta) {
+        if (meta == _emptyMeta) {
+            NSLog(@"meta not found: %@", ID);
+            return nil;
+        }
+    } else {
+        // first access, try to load from local storage
+        meta = [self _loadMetaForID:ID];
+        if (meta) {
+            // no need to check meta again
+            [_caches setObject:meta forKey:ID];
+        } else {
+            // meta not found
+            [_caches setObject:_emptyMeta forKey:ID];
+        }
+    }
     return meta;
 }
 
@@ -82,10 +94,10 @@
         NSAssert(false, @"meta not match ID: %@, %@", ID, meta);
         return NO;
     }
-//    if (![self _cacheMeta:meta forID:ID]) {
-//        NSAssert(false, @"failed to cache meta for ID: %@, %@", ID, meta);
-//        return NO;
-//    }
+    if (![self _cacheMeta:meta forID:ID]) {
+        NSAssert(false, @"failed to cache meta for ID: %@, %@", ID, meta);
+        return NO;
+    }
     NSString *path = [self _filePathWithID:ID];
     if ([self fileExistsAtPath:path]) {
         NSLog(@"meta already exists: %@", path);
