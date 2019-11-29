956a41a1105701d1006bfefd19b51f969ca49d12
Moky <albert.moky@gmail.com>
2019-11-29 15:01:08 +0800

    project SDK init

---
+++ sdk/DIMSDK/DIMSDKTests/DIMSDKTests.m
@@ -0,0 +1,37 @@
+//
+//  DIMSDKTests.m
+//  DIMSDKTests
+//
+//  Created by Albert Moky on 2019/11/29.
+//  Copyright © 2019 Albert Moky. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DIMSDKTests : XCTestCase
+
+@end
+
+@implementation DIMSDKTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
